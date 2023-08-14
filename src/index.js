05e0e3126efa894dbeef67e08cf503bc29f740b9
elat <elat.webdev@gmail.com>
2023-08-14 12:48:59 +0200

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('hi')
+
+//Build a node class/factory
+ 
+const createNode = (value) => {
+    const node = {
+        value: null,
+        next: null
+    }
+    return node;
+}
+
+const tree = (array) => {
+    //should have a root attribute - that uses the return value of buildtree()
+
+    const buildTree = () => {
+        // Takes array of data and returns balanced BST
+        // Should return the level 0 node
+
+    }
+
+    
+}
