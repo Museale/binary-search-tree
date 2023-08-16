d2fe88e2c1d4fca3d4aba0049a87e9eb17947277
elat <elat.webdev@gmail.com>
2023-08-16 16:59:33 +0200

    further changes buildtree func

--- src/index.js
+++ src/index.js
@@ -12,66 +12,24 @@ const createNode = (value) => {
 }
 
 const tree = (array) => {
-    //should have a root attribute - that uses the return value of buildtree()
-    
+  const rootNode = buildTree(array);
+  return rootNode;
+}
 
-    // const getRoot = (array) => {
-    //     //find middle
-    //     //root node is middle el of sorted array
-    //     let start = 0;
-    //     let end = array.length -1;
+const buildTree = (array, start = 0, end = array.length -1) => {
+  if (start > end) return null;
 
-    //     const mid = Math.floor(array.length/2)
-    //     const left = [];
-    //     const right = [];
+  const mid = Math.floor((start + end) / 2) ;
+  console.log(mid)
 
-    //     console.log(mid)
-    //     for (let i = 0; i < array.length; i++) {
-    //         if(array[i] < mid) {
-    //             left.push(array[i])
-    //         }
-    //         else if(array[i] > mid) {
-    //             right.push(array[i])
-    //         }
-            
-    //     }
-    //     console.log(left, right)
-    //     return mid;
-    // };
+  const root = createNode(array[mid]);
 
-    // getRoot(array)
-    
-    const buildTree = (array, start, end) => {
-
-      if(start > end){
-        return null;
-      }
-
-      let mid = Math.floor(array.length/2);
-      console.log(mid)
-      let rootNode = createNode(mid);
-
-      rootNode.left = buildTree(array, start, mid -1)
-      rootNode.right = buildTree(array, mid +1, end);
+  root.left = buildTree(array, start, mid -1);
+  root.right = buildTree(array, mid +1, end);
 
 
-      return root;
-
-      //create a node from root
-      //calculate mid of left subarray make it root of left subtree
-      //calculate mid of right subarray and make it root of right subtree
-
-      //height of left subrtee and right subtree should not differ by more than one
-       
-  
-
-        };
-        buildTree(array);
-       
-
-    
-};
-
+  return root;
+}
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
@@ -87,4 +45,5 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 
 
-tree([1, 2, 3, 4, 5, 6, 7])
+
+prettyPrint(tree([1,2,3,4,5,6,7,8,9,10]))
