9830a8057be1ee721ad2b3042a9bb6582298a71b
elat <elat.webdev@gmail.com>
2023-08-17 11:34:56 +0200

    add insert node func

--- src/index.js
+++ src/index.js
@@ -20,7 +20,10 @@ const tree = (array) => {
     }
   });
 
+  sortedArr.sort((a, b) => a < b ? -1 : 1);
+
   const rootNode = buildTree(sortedArr)
+
 
   return rootNode;
 }
@@ -39,6 +42,30 @@ const buildTree = (array, start = 0, end = array.length -1) => {
   return root;
 }
 
+const insertNode = (node, nodeToInsert) => {
+  //Insert a node in the tree
+  if (node === null) return;
+console.log(nodeToInsert.data, node.data)
+  // if value is less than root -- go to left subtree
+  // else go to right subtree
+  if (node !== null) {
+    if(node.data < nodeToInsert.data) {
+      insertNode(node.right, nodeToInsert)
+
+      if(node.right === null) {
+        node.right = nodeToInsert;
+      }
+    } else {
+      insertNode(node.left, nodeToInsert)
+      console.log(node)
+      if(node.left === null) {
+        node.left = nodeToInsert;
+      }
+    }
+ 
+  }
+  
+}
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
     return;
@@ -54,5 +81,13 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 
 
 const newTree = tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const toBeInsertedNode = createNode(30);
+const secondInsert = createNode(6);
+
+prettyPrint(newTree)
+
+insertNode(newTree, toBeInsertedNode)
+insertNode(newTree, secondInsert)
+
 
 prettyPrint(newTree)
