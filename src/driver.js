774ffb03c0b2841415487d7d99a9bd962cbf08cf
elat <elat.webdev@gmail.com>
2023-09-14 12:55:51 +0200

    moved driver functionality

--- src/driver.js
+++ src/driver.js
@@ -1,5 +1,28 @@
 import { BST } from '.'
+import { prettyPrint } from './pretty_print';
 
 export const driver = () => {
 
-}
+    const tree = BST([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+    tree.insertNode(2);
+    tree.insertNode(300)
+    tree.insertNode(559)
+    tree.insertNode(12930)
+    tree.insertNode(0)
+    tree.insertNode(-3234)
+    tree.insertNode(6)
+    tree.insertNode(129423)
+    tree.insertNode(121239148)
+    tree.deleteNode(6345);
+    tree.findNode(9);
+    tree.levelOrder();
+    tree.inOrder();
+    tree.preOrder();
+    tree.postOrder();
+    console.log('Height: ', tree.heightOfTree());
+    console.log('Depth: ', tree.depth(1))
+    console.log('IsBalanced: ', tree.isBalanced())
+    tree.reBalance();
+    prettyPrint(tree.root);    
+};
